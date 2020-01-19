31b7a186c81010b726fe7b0966f08f04431b304b
kelvin9314 <kelvin9314@gmail.com>
2020-01-19 16:56:12 +0800

    Updates

---
+++ precache-manifest.078d5b6ba0211fa9c3487295a2ee72cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9706c491e1c7ff023488e19f939a6e52",
+    "url": "/react-redux-saga-movie-list/index.html"
+  },
+  {
+    "revision": "731cfc26591ca16d353c",
+    "url": "/react-redux-saga-movie-list/static/css/main.3521999f.chunk.css"
+  },
+  {
+    "revision": "3f02c419be9307e3b352",
+    "url": "/react-redux-saga-movie-list/static/js/2.b45f48f0.chunk.js"
+  },
+  {
+    "revision": "7d66cbb52292c6d86ff795a004c53661",
+    "url": "/react-redux-saga-movie-list/static/js/2.b45f48f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "731cfc26591ca16d353c",
+    "url": "/react-redux-saga-movie-list/static/js/main.83033f82.chunk.js"
+  },
+  {
+    "revision": "4ab6dde3b2f11c01443a",
+    "url": "/react-redux-saga-movie-list/static/js/runtime-main.728e7938.js"
+  }
+]);
